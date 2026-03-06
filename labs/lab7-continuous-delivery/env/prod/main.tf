c26b24e9e6f4eb00ff9a884a03e00127671100d2
GrandChevreuil <138470548+GrandChevreuil@users.noreply.github.com>
2026-03-06 15:55:34 +0000

    feat: add prod environment (lz-prod-cd)

---
+++ labs/lab7-continuous-delivery/env/prod/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "lz_s3" {
+  source  = "app.terraform.io/tfc-lab-mathis/s3_lz/aws"
+  version = "1.0.0"
+
+  name        = "lz-lab7-mathis-bucket"
+  environment = "prod"
+}
