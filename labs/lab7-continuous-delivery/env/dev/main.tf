b05adeed8746f3863af38b42cf3bf0bbcd55d638
GrandChevreuil <138470548+GrandChevreuil@users.noreply.github.com>
2026-03-06 15:44:06 +0000

    fix: revert module name to lz_s3

--- labs/lab7-continuous-delivery/env/dev/main.tf
+++ labs/lab7-continuous-delivery/env/dev/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-module "lz_s3-test-changement-nom-module" {
+module "lz_s3" {
   source  = "app.terraform.io/tfc-lab-mathis/s3_lz/aws"
   version = "1.0.0"
 
